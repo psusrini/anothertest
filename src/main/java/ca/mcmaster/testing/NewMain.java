322ab67f288a57e095636d5d72be66b7cbfdd924
tamvadss <tamvadss@bus-analytics02.ads.mcmaster.ca>
2022-11-13 10:20:46 -0500

    testing

--- src/main/java/ca/mcmaster/testing/NewMain.java
+++ src/main/java/ca/mcmaster/testing/NewMain.java
@@ -5,11 +5,35 @@
  */
 package ca.mcmaster.testing;
 
+import static java.lang.System.exit;
+import static org.apache.log4j.Level.WARN;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.apache.log4j.RollingFileAppender;
+
 /**
  *
  * @author tamvadss
  */
 public class NewMain {
+     
+    static {
+        Logger logger=Logger.getLogger(NewMain.class);
+        logger.setLevel(WARN);
+        PatternLayout layout = new PatternLayout("%5p  %d  %F  %L  %m%n");     
+        try {
+            RollingFileAppender rfa =new  
+                RollingFileAppender(layout, NewMain.class.getSimpleName());
+            rfa.setMaxBackupIndex(60);
+            logger.addAppender(rfa);
+            logger.setAdditivity(false);            
+             
+        } catch (Exception ex) {
+            ///
+            System.err.println("Exit: unable to initialize logging"+ex);       
+            exit(1);
+        }
+    }
 
     /**
      * @param args the command line arguments
